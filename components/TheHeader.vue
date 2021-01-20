cc22678f61e1ebf6468a36499891d4b8e0ff64d2
pcweb <contact@pcweb-conseils.com>
2021-01-20 12:43:05 +0100

    reglage  header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="relative z-10 w-full h-24 px-12 bg-white flex justify-between">
-    <a
-      href="#"
-      class="block h-full w-12 flex items-center text-lg md:text-2xl font-bold tracking-widest text-gray-700 uppercase hover:text-gray-400"
-      >skyscraper</a
-    >
+  <div class="relative z-10 w-full h-60 px-12 bg-white flex justify-between">
+    <nuxt-link
+      to="/"
+      class="block h-full flex items-center text-lg md:text-2xl font-bold tracking-widest text-gray-700 uppercase hover:text-gray-400"
+      ><Logo
+    /></nuxt-link>
 
     <a
       href="#"
-      class="relative block px-3 h-32 bg-blue-900 text-white text-center tracking-widest uppercase text-xs font-bold pt-8 pb-6 flex flex-col items-center justify-between hover:bg-gray-900"
+      class="relative block px-3 bg-gray-700 text-white text-center tracking-widest uppercase text-xs font-bold pt-8 pb-6 flex flex-col items-center justify-between hover:bg-gray-900"
     >
       <span>menu</span>
       <div class="flex items-end justify-center">
