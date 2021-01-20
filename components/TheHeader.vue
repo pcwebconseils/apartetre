083c6bb9ec6f51bf409bd74ea96c0988cb306708
pcweb <contact@pcweb-conseils.com>
2021-01-20 10:02:26 +0100

    home nouvelle version

---
+++ components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative z-10 w-full h-24 px-12 bg-white flex justify-between">
+    <a
+      href="#"
+      class="block h-full w-12 flex items-center text-lg md:text-2xl font-bold tracking-widest text-gray-700 uppercase hover:text-gray-400"
+      >skyscraper</a
+    >
+
+    <a
+      href="#"
+      class="relative block px-3 h-32 bg-blue-900 text-white text-center tracking-widest uppercase text-xs font-bold pt-8 pb-6 flex flex-col items-center justify-between hover:bg-gray-900"
+    >
+      <span>menu</span>
+      <div class="flex items-end justify-center">
+        <span class="block h-5 w-1 bg-yellow-600"></span>
+        <span class="block h-8 w-1 bg-yellow-600 mx-1"></span>
+        <span class="block h-5 w-1 bg-yellow-600"></span>
+      </div>
+    </a>
+  </div>
+</template>
